d5a09d194fc36a67ccc7487ccd33328364373207
Hassan Mata <ajdourhassan29@gmail.com>
2020-08-04 22:21:23 +0200

    save all changes

--- web_project/dieseshop/models.py
+++ web_project/dieseshop/models.py
@@ -3,8 +3,8 @@ from django.db import models
 #Create your models here.
 class Product (models.Model) :
   name  = models.CharField(max_length= 15)
-  price = models.IntegerField()
   about = models.TextField(max_length=60)
-
+  price = models.IntegerField()
+  
   def __str__(self):
-    return 'user : ' + self.name
+    return " Product's name  : " + self.name

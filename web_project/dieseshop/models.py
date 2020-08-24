edc2afab95a637bdf85051e1a33d4d334f307dde
Hassan Mata <ajdourhassan29@gmail.com>
2020-08-24 11:19:52 +0200

    save all changes

--- web_project/dieseshop/models.py
+++ web_project/dieseshop/models.py
@@ -1,10 +1,49 @@
 from django.db import models
+from django.urls import reverse
 
-#Create your models here.
-class Product (models.Model) :
-  name  = models.CharField(max_length= 15)
-  about = models.TextField(max_length=60)
-  price = models.IntegerField()
-  
+# Create your models here.
+class Category (models.Model):
+  name = models.CharField(max_length=250, unique=True)
+  slug = models.SlugField(max_length=250 , unique=True)
+  image = models.ImageField(upload_to="./images")
+  description = models.TextField(max_length=200 , blank=True , null=True)
+
+  class Meta:
+    ordering = ('name',)
+    verbose_name = 'category'
+    verbose_name_plural = 'categories'
+
+# we gave here url/ or id as slug depends on ('products_by_category' = Url)
+  def get_url(self):
+    return reverse('products_by_category', args=[self.slug])
+
   def __str__(self):
-    return " Product's name  : " + self.name
+    return self.name
+
+class Product(models.Model):
+  category = models.ForeignKey(Category, on_delete=models.CASCADE)
+  name = models.CharField(max_length=20 , unique=True)
+  slug = models.SlugField(max_length=250 , unique=True)
+  description = models.TextField(max_length=200 , blank=True , null=True)
+  price = models.DecimalField(max_digits=10 , decimal_places=2)
+  image = models.ImageField(upload_to="./images")
+  stock = models.IntegerField()
+  available = models.BooleanField(default=True)
+  created_date = models.DateTimeField(auto_now_add=True)
+  updated = models.DateTimeField(auto_now=True)
+
+
+  class Meta:
+    ordering = ('name',)
+    verbose_name = 'product'
+    verbose_name_plural = 'products'
+
+  def get_url(self) :
+    return reverse('product_detail' , args=[self.category.slug , self.slug])
+
+  def __str__(self):
+    return self.name
+
+
+
+
--- web_project/dieseshop/urls.py
+++ web_project/dieseshop/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('product' , views.product , name='product'),
-]
+   path('' , views.home , name='home'),
+   path('about' , views.about , name='about'),
+   path('category/<slug:category_slug>' , views.home , name='products_by_category'),
+   path('category/<slug:category_slug>/<slug:product_slug>', views.productpage, name="product_detail"),
+   path("cart" , views.cart , name="cart"),
+
+] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
